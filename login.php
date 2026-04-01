fa363573019c1bd90922a9ab8b40757461545930
Roberto <robertocoesantos@gmail.com>
2026-04-01 08:39:45 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<form action="validar_login.php" method="POST">
+    Email: <input type="email" name="email"><br><br>
+    Senha: <input type="password" name="senha"><br><br>
+
+    <button type="submit">Entrar</button>
+</form>
+
+</body>
+</html>
