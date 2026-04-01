83740a6de1357623c0bb9b01be9cf7721fa033fa
Roberto <robertocoesantos@gmail.com>
2026-04-01 08:33:05 -0300

    Add cadastro.php for user registration form

---
+++ cadastro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro</title>
+</head>
+<body>
+
+<h2>Cadastro</h2>
+
+<form action="salvar_cadastro.php" method="POST">
+    Nome: <input type="text" name="nome"><br><br>
+    Email: <input type="email" name="email"><br><br>
+    Senha: <input type="password" name="senha"><br><br>
+
+    <button type="submit">Cadastrar</button>
+</form>
+
+</body>
+</html>
